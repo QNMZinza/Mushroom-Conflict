44b573fb157720f543102029cbf0f1afe1d2606f
QNM <quang.nm@zinza.com.vn>
2020-06-03 10:27:20 +0700

    Commit

---
+++ Assets/Script/BannerController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+using GoogleMobileAds.Api;
+using GoogleMobileAds.Common;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+
+public class BannerController : MonoBehaviour
+{
+    private BannerView bannerView;
+
+    public UnityEvent OnAdLoadedEvent;
+    public UnityEvent OnAdFailedToLoadEvent;
+    public UnityEvent OnAdOpeningEvent;
+    public UnityEvent OnAdFailedToShowEvent;
+    public UnityEvent OnUserEarnedRewardEvent;
+    public UnityEvent OnAdClosedEvent;
+    public UnityEvent OnAdLeavingApplicationEvent;
+
+    public void Start()
+    {
+        MobileAds.SetiOSAppPauseOnBackground(true);
+
+        List<String> deviceIds = new List<String>() { AdRequest.TestDeviceSimulator };
+
+        // Add some test device IDs (replace with your own device IDs).
+#if UNITY_IPHONE
+        deviceIds.Add("96e23e80653bb28980d3f40beb58915c");
+#elif UNITY_ANDROID
+        deviceIds.Add("75EF8D155528C04DACBBA6F36F433035");
+#endif
+
+        // Configure TagForChildDirectedTreatment and test device IDs.
+        RequestConfiguration requestConfiguration =
+            new RequestConfiguration.Builder()
+            .SetTagForChildDirectedTreatment(TagForChildDirectedTreatment.Unspecified)
+            .SetTestDeviceIds(deviceIds).build();
+
+        MobileAds.SetRequestConfiguration(requestConfiguration);
+
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(HandleInitCompleteAction);
+    }
+
+    private void HandleInitCompleteAction(InitializationStatus initstatus)
+    {
+        // Callbacks from GoogleMobileAds are not guaranteed to be called on
+        // main thread.
+        // In this example we use MobileAdsEventExecutor to schedule these calls on
+        // the next Update() loop.
+        MobileAdsEventExecutor.ExecuteInUpdate(() => {
+            RequestBannerAd();
+        });
+    }
+
+    public void RequestBannerAd()
+    {
+        // These ad units are configured to always serve test ads.
+#if UNITY_EDITOR
+        string adUnitId = "unused";
+#elif UNITY_ANDROID
+        string adUnitId = "ca-app-pub-6308692614428475/1297590138";
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+        // Clean up banner before reusing
+        if (bannerView != null)
+        {
+            bannerView.Destroy();
+        }
+
+        // Create a 320x50 banner at top of the screen
+        bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Top);
+
+        // Add Event Handlers
+        bannerView.OnAdLoaded += (sender, args) => OnAdLoadedEvent.Invoke();
+        bannerView.OnAdFailedToLoad += (sender, args) => OnAdFailedToLoadEvent.Invoke();
+        bannerView.OnAdOpening += (sender, args) => OnAdOpeningEvent.Invoke();
+        bannerView.OnAdClosed += (sender, args) => OnAdClosedEvent.Invoke();
+        bannerView.OnAdLeavingApplication += (sender, args) => OnAdLeavingApplicationEvent.Invoke();
+
+        // Load a banner ad
+        bannerView.LoadAd(CreateAdRequest());
+        Debug.Log("Request Banner");
+    }
+
+    public void DestroyBannerAd()
+    {
+        if (bannerView != null)
+        {
+            bannerView.Destroy();
+        }
+    }
+
+    private AdRequest CreateAdRequest()
+    {
+        return new AdRequest.Builder()
+            .AddTestDevice(AdRequest.TestDeviceSimulator)
+            .AddTestDevice("0123456789ABCDEF0123456789ABCDEF")
+            .AddKeyword("unity-admob-sample")
+            .TagForChildDirectedTreatment(false)
+            .AddExtra("color_bg", "9B30FF")
+            .Build();
+    }
+
+}
